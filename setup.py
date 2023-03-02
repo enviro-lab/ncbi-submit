511a645ac6b992fe499c806803a3c8687c7c711a
Sam Kunkleman <skunklem@uncc.edu>
2023-03-02 10:01:35 -0500

    more pandas version shuffling

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ URL = 'https://github.com/enviro-lab/ncbi_interact'
 EMAIL = 'skunklem@uncc.edu'
 AUTHOR = 'Sam Kunkleman'
 REQUIRES_PYTHON = '>=3.8'
-VERSION = '0.2.7'
+VERSION = '0.2.8'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
