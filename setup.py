917dc85ea4c43665899ecfc5ced507d649bff2b0
Sam Kunkleman <skunklem@uncc.edu>
2023-03-02 09:58:02 -0500

    more biopython version shuffling

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ URL = 'https://github.com/enviro-lab/ncbi_interact'
 EMAIL = 'skunklem@uncc.edu'
 AUTHOR = 'Sam Kunkleman'
 REQUIRES_PYTHON = '>=3.8'
-VERSION = '0.2.5'
+VERSION = '0.2.7'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
